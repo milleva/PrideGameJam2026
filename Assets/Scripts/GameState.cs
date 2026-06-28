3746716936600ddc622b1a5677f8973fdf15e0dd
Evan Miller <emiller.arkko@gmail.com>
2026-06-28 11:42:14 +0300

    Add global static game state class

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,13 @@
+public static class GameState
+{
+    public static int wealthPoints = 0;
+    public static int susPoints = 0;
+    public static int pedoPoints = 0;
+
+    public static void ResetGameState()
+    {
+        wealthPoints = 0;
+        susPoints = 0;
+        pedoPoints = 0;
+    }
+}
